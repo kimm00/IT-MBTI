755d3a75a1946fd29364c4f28298872e5f681d0e
Park Shinhyung <23102009@seoultech.ac.kr>
2024-12-10 14:53:35 +0900

    style: fix design color and location value

--- it-mbti/src/pages/StartPage.js
+++ it-mbti/src/pages/StartPage.js
@@ -7,13 +7,16 @@ const Page = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
+  height: 100vh;
+  background-color: #f4f4f9;
 `;
 const Container = styled.div`
   position: relative;
   width: 1296px;
   height: 863px;
-
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
 `;
 
 const Title = styled.h1`
@@ -32,7 +35,7 @@ position: absolute;
   left: 79px;
   font-size: 30px;
   color: #333;
-
+  margin-bottom: 20px;
 `;
 const Cat = styled.img`
 
@@ -41,13 +44,28 @@ const Cat = styled.img`
   left: 793px;
   width: 503px;
   height: 583px;
-
+  object-fit: cover;
 `;
 
 const StartButton = styled.button`
 
   position: absolute;
   top: 555px;
+  left: 71px;
+
+  width: 297px;
+  height: 72px;
+
+  font-size: 24px;
+  color: white;
+  background-color: #6c63ff;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #574bff;
+  }
 `;
 
 function StartPage() {
