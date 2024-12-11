db95ad3d7ad7ec850e7859287cba08e3a6377a89
yuyoung <hyyy8810@gmail.com>
2024-12-11 23:32:09 +0900

    feat: update detail.js

--- it-mbti/src/components/Detail.js
+++ it-mbti/src/components/Detail.js
@@ -1,3 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import results from "../data/results.json";
+
+const DetailContainer = styled.div`
+  position: relative;
+  width: 530px;
+  height: 470px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+`;
+
+const Work = styled.h3`
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  font-size: 20px;
+  font-weight: bold;
+  color: #333;
+`;
+
+const WorkDescription = styled.p`
+  position: absolute;
+  top: 60px;
+  left: 25px;
+  right: 20px;
+  font-size: 16px;
+  color: #555;
+  line-height: 1.5;
+`;
+
+
 function Detail({ type }) {
     const result = results.find((item) => item.type === type) || {};
   
