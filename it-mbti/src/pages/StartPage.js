7ec5e169dc58ca9ed301d9717de42c19b42430b9
Haneol LEE <skqieum@gmail.com>
2024-12-15 22:18:43 +0900

    design: update StartPage.js

--- it-mbti/src/pages/StartPage.js
+++ it-mbti/src/pages/StartPage.js
@@ -2,35 +2,35 @@ import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
-const Page = styled.div`
+const Page = styled.div
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
   background: linear-gradient(180deg, #E8EAF3 10%, #A0B7E1 40%, #4A79D1 90%);
-`;
-const Container = styled.div`
+;
+const Container = styled.div
   position: relative;
   width: 100%;
   height: 863px;
   background: rgba(232, 234, 243, 0);
   border-radius: 10px;
-`;
-const Title2 = styled.h1`
+;
+const Title2 = styled.h1
   position: absolute;
-  top: 100px;
+  top: 141px;
   left: 79px;
   font-size: 100px;
   color: Black;
   margin-bottom: 20px;
   font-family: "sans-serif";
   font-weight: 900;
-`;
+;
 
 const Title = styled.h1`
   position: absolute;
-  top: 180px;
+  top: 230px;
   left: 79px;
   font-size: 100px;
   color: Black;
@@ -42,42 +42,43 @@ const Title = styled.h1`
 const Description = styled.h1`
   position: absolute;
   width: 700px;
-  top: 280px;
+  top: 390px;
   left: 79px;
   font-size: 30px;
   color: black;
-  margin-bottom: 50px;
+  margin-top: 40px;
   font-family: "GmarketSans";
   font-weight: 500;
 `;
 
-const Deco2 = styled.img`
+const Deco2 = styled.img
  position: absolute;
   top: 123px;
   left: 793px;
   width: 648px;
   height: 648px;
   object-fit: cover;
-`;
+;
 
-const StartButton = styled.button`
+const StartButton = styled.button
+
   position: absolute;
-  top: 520px;
+  top: 600px;
   left: 71px;
   width: 297px;
   height: 72px;
-  font-size: 28px;
+  font-size: 24px;
   color: white;
   background: black;
   border: none;
   border-radius: 8px;
   cursor: pointer;
-  font-weight: 600;
+  font-weight: 500;
 
   &:hover {
     background-color: #574bff;
   }
-`;
+;
 
 function StartPage() {
   const navigate = useNavigate();
