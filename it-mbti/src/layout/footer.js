f5ed0a1ca7fd3cbb63eab353380797f5f10c05dd
Haneol LEE <skqieum@gmail.com>
2024-12-15 20:08:59 +0900

    design: update footer.js

--- it-mbti/src/layout/footer.js
+++ it-mbti/src/layout/footer.js
@@ -6,7 +6,7 @@ const FooterSection = styled.footer`
   bottom: 0;
   left: 0;
   width: 100%;
-  height: 120px; /* 높이를 120px로 설정 */
+  height: 120px;
   font-family: GmarketSans;
   display: flex;
   justify-content: space-between;
@@ -18,9 +18,9 @@ const FooterSection = styled.footer`
   .logo {
     display: flex;
     align-items: center;
-    margin-left: 10px; /* 좌측 여백 */
+    margin-left: 10px;
     img {
-      width: 80px; /* 로고 크기 */
+      width: 90px;
       height: auto;
     }
   }
@@ -35,18 +35,18 @@ const FooterSection = styled.footer`
 
   .text1,
   .text2 {
-    font-size: 25px; /* 폰트 크기를 25px로 설정 */
-    font-weight: 600; /* 폰트 두께를 600으로 설정 */
-    margin-right: 20px; /* 두 텍스트 사이 간격 설정 */
+    font-size: 25px;
+    font-weight: 600;
+    margin-right: 300px;
   }
 
   .text3 {
-    font-size: 14px; /* 줄바꿈 텍스트 크기 */
+    font-size: 14px;
     font-weight: 400;
-    white-space: pre-line; /* 줄바꿈 적용 */
-    line-height: 1.6; /* 줄 간격 설정 */
-    text-align: right; /* 오른쪽 정렬 */
-    margin-left: auto; /* 오른쪽으로 위치 고정 */
+    white-space: pre-line;
+    line-height: 1.6;
+    text-align: right;
+    margin-left: auto;
   }
 `;
 
@@ -57,7 +57,7 @@ const Footer = () => {
         <img src="/img/seoultech.svg" alt="main-logo" />
       </div>
       <div className="contents">
-        {/* text1과 text2를 한 줄에 위치 */}
+        {}
         <div>
           <span className="text1">Seoul National University of Science and Technology</span>
           <span className="text2">ITM OSS PROJECT</span>
